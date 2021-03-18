9c26b255aac308ae4b54e47d244764c0a52aade7
İbrahim Culha <ibrahimculha1996@gmail.com>
2021-03-18 06:25:39 +0300

    first commit

---
+++ blog/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+
+  path('',views.home, name="home"),
+  path('contact.html',views.contact, name="contact"),
+  path('pricing.html',views.pricing, name="pricing"),
+  path('about.html',views.about, name="about"),
+  path('service.html',views.service, name="service"),
+  path('appointment.html',views.appointment, name="appointment"),
+
+
+
+
+  ]
